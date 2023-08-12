927c4f7187be24f6215e4a227e7907ef4b2f5532
Meaghan <meaghan.sharrard89@gmail.com>
2023-08-12 16:41:47 -0700

    finish

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
-// Code your solutions in this file
+// Assignment 1
+
+const newNames = [];
+
+function writeCards(names, event) {
+    for (let i = 0; i < names.length; i++) {
+        newNames.push(`Thank you, ${names[i]}, for the wonderful ${event} gift!`);
+    }
+    return newNames;
+}
+
+//Assignment 2
+
+function countDown(count) {
+    while (count >= 0) {
+        console.log(count--);
+    }
+}
